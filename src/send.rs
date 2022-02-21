74c86b88bd64ccd0a8211472b9bd86fe2d0632ed
David Vernet <void@manifault.com>
2022-02-21 10:16:00 -0800

    Have child git send-email process inherit fd's from rust

--- src/send.rs
+++ src/send.rs
@@ -1,10 +1,11 @@
 use std::path::Path;
-use std::process::{Command};
+use std::process::{Command, Stdio};
 
 /// Send the specified patch to the given list of recipients.
 pub fn send_patch(recipients: &[String], patch_path: &Path) {
-    let mut command_args = vec!["send-email", "--dry-run"];
+    let mut command_args = vec!["send-email"];
     for recipient in recipients {
+        log::debug!("to: {}", recipient);
         command_args.extend(["--to", recipient]);
     }
     command_args.push(patch_path.to_str().unwrap());
@@ -12,16 +13,13 @@ pub fn send_patch(recipients: &[String], patch_path: &Path) {
     // Invoke git send-email.
     let output = Command::new("git")
                          .args(&command_args)
+                         .stderr(Stdio::inherit())
+                         .stdout(Stdio::inherit())
+                         .stdin(Stdio::inherit())
                          .output()
                          .expect("Failed to execute git send-email");
     if !output.status.success() {
         let stderr = String::from_utf8(output.stderr).expect("Failed to parse stderr");
         panic!("({} | {}): Failed to invoke git send-email {}", output.status, stderr, command_args.join(" "));
     }
-
-
-    String::from_utf8(output.stdout)
-        .expect("Failed to parse stdout")
-        .lines()
-        .for_each(|x| println!("{:?}", x));
 }
